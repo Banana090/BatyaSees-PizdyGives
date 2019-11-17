a450b120f36fcd49d3aa59e16d404b6dcc5176a5
glebster51 <glebster51@rambler.ru>
2019-11-17 13:48:42 +0800

    болчка и ковер

---
+++ Assets/!MainProject/G51/Script/g51_Bar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class g51_Bar : MonoBehaviour
+{
+    public Transform scaler;
+    [Range(0f, 1f)] public float value;
+
+    private void LateUpdate()
+    {
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        value = Mathf.Clamp(value, 0f, 1f);
+        if (scaler)
+            scaler.localScale = new Vector3(value, 1f, 1f);
+    }
+}
